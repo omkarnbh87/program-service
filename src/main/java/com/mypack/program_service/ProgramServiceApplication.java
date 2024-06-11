6bfc1f2e8d8d55db710d845f5c784a65682cd89e
Omkarnbh87 <contact.omkar87@gmail.com>
2024-06-11 14:17:42 +0530

    First version commit with initial setup

---
+++ src/main/java/com/mypack/program_service/ProgramServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mypack.program_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramServiceApplication.class, args);
+	}
+
+}
